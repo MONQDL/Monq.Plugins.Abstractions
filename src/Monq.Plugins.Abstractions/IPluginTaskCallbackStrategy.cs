09c915dd04dbac8aa17a070f23b3b790030ea31e
2nnar <lobovyroslav@gmail.com>
2024-03-12 16:32:10 +0300

    Add task strategy with callback, remove redundant postprocessor strategy

--- src/Monq.Plugins.Abstractions/IPluginTaskCallbackStrategy.cs
+++ src/Monq.Plugins.Abstractions/IPluginTaskCallbackStrategy.cs
@@ -1,19 +1,21 @@
 ﻿namespace Monq.Plugins.Abstractions;
 
 /// <summary>
-/// Стратегия выполнения задания плагина.
+/// Стратегия выполнения задания плагина с функцией обратного вызова.
 /// </summary>
-public interface IPluginTaskStrategy
+public interface IPluginTaskCallbackStrategy
 {
     /// <summary>
     /// Выполнить задание.
     /// </summary>
     /// <param name="variables">Переменные задания плагина.</param>
     /// <param name="securedVariables">Список защищённых переменных.</param>
+    /// <param name="callback">Функция обратного вызова для обработки результата выполнения задания.</param>
     /// <param name="cancellationToken">Токен для отмены выполнения задания.</param>
-    /// <returns>Выходные данные задания.</returns>
-    Task<IDictionary<string, object?>> Run(
+    /// <returns><see cref="Task"/>, показывающий выполнение операции.</returns>
+    Task Run(
         IDictionary<string, object?> variables,
         IEnumerable<string> securedVariables,
+        Func<IDictionary<string, object?>, Task> callback,
         CancellationToken cancellationToken);
 }
