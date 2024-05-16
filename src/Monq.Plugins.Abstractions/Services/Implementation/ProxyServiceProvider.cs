f5e9135b10a543983ce450bc0ad0d0b54f36e205
2nnar <lobovyroslav@gmail.com>
2024-05-17 02:41:40 +0300

    Add proxy service provider

---
+++ src/Monq.Plugins.Abstractions/Services/Implementation/ProxyServiceProvider.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Monq.Plugins.Abstractions.Services.Implementation;
+
+/// <summary>
+/// Реализация интерфейса прокси-провайдера сервисов.
+/// </summary>
+public class ProxyServiceProvider : IServiceProvider, IProxyServiceProvider
+{
+    readonly IServiceProvider _serviceProvider;
+
+    /// <summary>
+    /// Конструктор реализации интерфейса прокси-провайдера сервисов.
+    /// </summary>
+    public ProxyServiceProvider(
+        IServiceCollection serviceCollection)
+    {
+        _serviceProvider = serviceCollection.BuildServiceProvider();
+    }
+
+    /// <inheritdoc/>
+    public object? GetService(Type serviceType)
+        => _serviceProvider.GetService(serviceType);
+}
