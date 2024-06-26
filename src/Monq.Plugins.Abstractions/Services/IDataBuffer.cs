2b55cb7e9f5b35147be9d9e2ce1d51a3528e3abb
2nnar <lobovyroslav@gmail.com>
2024-06-27 01:50:28 +0300

    Add data buffer interface

---
+++ src/Monq.Plugins.Abstractions/Services/IDataBuffer.cs
@@ -0,0 +1,16 @@
+﻿namespace Monq.Plugins.Abstractions.Services;
+
+/// <summary>
+/// Интерфейс буфера данных.
+/// </summary>
+public interface IDataBuffer
+{
+    /// <summary>
+    /// Записать данные.
+    /// </summary>
+    /// <param name="pluginInstance">Название экземпляра плагина.</param>
+    /// <param name="data">Массив байтов.</param>
+    /// <param name="cancellationToken">Токен отмены операции.</param>
+    /// <returns><see cref="Task"/>, показывающий завершение операции.</returns>
+    Task Write(string pluginInstance, byte[] data, CancellationToken cancellationToken = default);
+}
