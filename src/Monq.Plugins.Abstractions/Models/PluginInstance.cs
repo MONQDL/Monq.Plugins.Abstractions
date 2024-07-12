a360d5ffa2c3db47306d04896eaa3dcbbc9e35c6
2nnar <lobovyroslav@gmail.com>
2024-07-12 16:25:05 +0300

    Add plugin instance class

---
+++ src/Monq.Plugins.Abstractions/Models/PluginInstance.cs
@@ -0,0 +1,49 @@
+﻿namespace Monq.Plugins.Abstractions.Models;
+
+/// <summary>
+/// Экземпляр плагина.
+/// </summary>
+public class PluginInstance
+{
+    /// <summary>
+    /// Название.
+    /// </summary>
+    public string Name { get; init; } = string.Empty;
+
+    /// <summary>
+    /// Ключ потока данных.
+    /// </summary>
+    public string StreamKey { get; init; } = string.Empty;
+
+    /// <summary>
+    /// Размер чанка в байтах.
+    /// </summary>
+    public int ChunkSize { get; init; }
+
+    /// <summary>
+    /// Тип буфера.
+    /// </summary>
+    public BufferTypes BufferType { get; init; } = BufferTypes.Memory;
+
+    /// <summary>
+    /// Разделитель записей.
+    /// </summary>
+    public byte[] Separator { get; init; } = Array.Empty<byte>();
+
+    /// <summary>
+    /// Дата получения последней записи.
+    /// </summary>
+    public DateTimeOffset LastRecordDate { get; private set; } = DateTimeOffset.UtcNow;
+
+    /// <summary>
+    /// Функция парсинга.
+    /// </summary>
+    public Func<byte[], Task<byte[]>>? HandleRecord { get; init; }
+
+    /// <summary>
+    /// Обновить дату получения последней записи.
+    /// </summary>
+    /// <param name="lastRecordDate">Дата последней записи.</param>
+    public void UpdateLastRecordDate(DateTimeOffset lastRecordDate)
+        => LastRecordDate = lastRecordDate;
+}
--- src/Monq.Plugins.Abstractions/Services/IDataBuffer.cs
+++ src/Monq.Plugins.Abstractions/Services/IDataBuffer.cs
@@ -10,38 +10,12 @@ public interface IDataBuffer
     /// <summary>
     /// Записать данные.
     /// </summary>
-    /// <param name="pluginInstance">Название экземпляра плагина.</param>
-    /// <param name="streamKey">Ключ потока данных.</param>
+    /// <param name="pluginInstance">Экземпляр плагина.</param>
     /// <param name="data">Массив байтов.</param>
-    /// <param name="chunkSize">Размер чанка в байтах.</param>
-    /// <param name="bufferType">Тип буфера.</param>
     /// <param name="cancellationToken">Токен отмены операции.</param>
     /// <returns><see cref="Task"/>, показывающий завершение операции.</returns>
     Task Write(
-        string pluginInstance,
-        string streamKey,
+        PluginInstance pluginInstance,
         byte[] data,
-        int chunkSize,
-        BufferTypes bufferType = BufferTypes.Memory,
-        CancellationToken cancellationToken = default);
-
-    /// <summary>
-    /// Записать данные с разделитилем.
-    /// </summary>
-    /// <param name="pluginInstance">Название экземпляра плагина.</param>
-    /// <param name="streamKey">Ключ потока данных.</param>
-    /// <param name="data">Массив байтов.</param>
-    /// <param name="chunkSize">Размер чанка в байтах.</param>
-    /// <param name="delimiter">Массив байтов разделителя.</param>
-    /// <param name="bufferType">Тип буфера.</param>
-    /// <param name="cancellationToken">Токен отмены операции.</param>
-    /// <returns><see cref="Task"/>, показывающий завершение операции.</returns>
-    Task WriteWithDelimiter(
-        string pluginInstance,
-        string streamKey,
-        byte[] data,
-        int chunkSize,
-        byte[] delimiter,
-        BufferTypes bufferType = BufferTypes.Memory,
         CancellationToken cancellationToken = default);
 }
