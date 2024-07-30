0d452822633fd472d5f05df610801dc550df96a7
2nnar <lobovyroslav@gmail.com>
2024-07-30 15:46:48 +0300

    Add string forgiving converter for system.text.json extensions

---
+++ src/Monq.Plugins.Abstractions/Converters/SystemTextJsonForgivingStringConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Buffers;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Monq.Plugins.Abstractions.Converters;
+
+internal class SystemTextJsonForgivingStringConverter : JsonConverter<string>
+{
+    public override string? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        => reader.TokenType switch
+        {
+            JsonTokenType.False => "false",
+            JsonTokenType.True => "true",
+            JsonTokenType.Number => Encoding.UTF8.GetString(reader.HasValueSequence ? reader.ValueSequence.ToArray() : reader.ValueSpan),
+            _ => reader.GetString(),
+        };
+
+    public override void Write(Utf8JsonWriter writer, string value, JsonSerializerOptions options)
+        => writer.WriteStringValue(value);
+}
