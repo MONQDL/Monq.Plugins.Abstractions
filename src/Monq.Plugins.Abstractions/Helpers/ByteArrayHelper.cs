9cc0785bc140fb3c8a6824fa59ae2954ac7dd3af
2nnar <lobovyroslav@gmail.com>
2024-12-05 20:37:58 +0300

    Add TryParseToJsonObject method in byte array helper

--- src/Monq.Plugins.Abstractions/Helpers/ByteArrayHelper.cs
+++ src/Monq.Plugins.Abstractions/Helpers/ByteArrayHelper.cs
@@ -1,4 +1,6 @@
-﻿using System.Text.Json;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 
 namespace Monq.Plugins.Abstractions.Helpers;
 
@@ -12,6 +14,11 @@ public static class ByteArrayHelper
     /// </summary>
     /// <param name="bytes">Массив байтов.</param>
     /// <returns>true, если массив представляет собой json объект, иначе - false.</returns>
+    /// <remarks>
+    /// Более оптимизированный вариант по сравнению
+    /// с <see cref="TryParseToJsonObject(byte[], out JsonObject?)"/>,
+    /// если не важно получение результирующего объекта.
+    /// </remarks>
     public static bool IsValidJsonObject(byte[] bytes)
     {
         try
@@ -21,6 +28,26 @@ public static class ByteArrayHelper
         }
         catch (JsonException)
         {
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Попытаться распарсить массив байтов в json объект.
+    /// </summary>
+    /// <param name="bytes">Массив байтов.</param>
+    /// <param name="jsonObj">Полученный json объект.</param>
+    /// <returns>true, если массив представляет собой json объект, иначе - false.</returns>
+    public static bool TryParseToJsonObject(byte[] bytes, [NotNullWhen(true)] out JsonObject? jsonObj)
+    {
+        try
+        {
+            jsonObj = JsonNode.Parse(bytes)!.AsObject();
+            return true;
+        }
+        catch (Exception)
+        {
+            jsonObj = null;
             return false;
         }
     }
