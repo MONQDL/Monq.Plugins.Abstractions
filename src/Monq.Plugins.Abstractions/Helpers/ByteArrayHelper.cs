4a0788a16110dc2163cd5c0d803887cf036f008e
2nnar <lobovyroslav@gmail.com>
2024-12-05 17:06:16 +0300

    Add byte array helper class

---
+++ src/Monq.Plugins.Abstractions/Helpers/ByteArrayHelper.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.Json;
+
+namespace Monq.Plugins.Abstractions.Helpers;
+
+/// <summary>
+/// Вспомогательный класс для работы с массивами байтов.
+/// </summary>
+public static class ByteArrayHelper
+{
+    /// <summary>
+    /// Проверить, является ли массив байтов валидном json объектом.
+    /// </summary>
+    /// <param name="bytes">Массив байтов.</param>
+    /// <returns>true, если массив представляет собой json объект, иначе - false.</returns>
+    public static bool IsValidJsonObject(byte[] bytes)
+    {
+        try
+        {
+            using var doc = JsonDocument.Parse(bytes);
+            return doc.RootElement.ValueKind == JsonValueKind.Object;
+        }
+        catch (JsonException)
+        {
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Убрать байты из <paramref name="trimChars"/> с начала и конца массива <paramref name="bytes"/>.
+    /// </summary>
+    /// <param name="bytes">Исходный массив байтов.</param>
+    /// <param name="trimChars">Байты, которые нужно убрать.</param>
+    /// <returns>Целевой массив.</returns>
+    public static ReadOnlySpan<byte> Trim(ReadOnlySpan<byte> bytes, params byte[] trimChars)
+    {
+        var start = 0;
+        var end = bytes.Length - 1;
+
+        while (start <= end && trimChars.Contains(bytes[start]))
+            start++;
+
+        while (end >= start && trimChars.Contains(bytes[end]))
+            end--;
+
+        return bytes.Slice(start, end - start + 1);
+    }
+}
